a44f6ca787464f10b1b4679bbae71c35ffee6750
Andre de Oliveira <oliveira.andrede@gmail.com>
2019-04-15 15:24:36 -0300

    Add react router main route

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 import Navbar from './layout/Navbar'
 import MainContainer from './layout/MainContainer';
-import PostItem from  './ui/PostItem';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 const AppContainer = styled.div`
   font-family: 'Press Start 2P', cursive;
 `;
 
+const Home = () => <div>Main component</div>
 
 const App = () => (
-  <AppContainer> 
-    <Navbar />
-    <MainContainer>
-      <PostItem />
-    </MainContainer>
-  </AppContainer>
+  <Router>
+    <AppContainer> 
+      <Navbar />
+      <MainContainer>
+        <Route exact path="/" component={Home} />
+      </MainContainer>
+    </AppContainer>
+  </Router>
 );
 
 export default App;
